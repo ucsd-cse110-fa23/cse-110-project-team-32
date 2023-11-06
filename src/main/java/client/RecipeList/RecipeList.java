a945cd42fff58e6e76e0abb1380fd4b86e5369da
Alexander Lee <alexanderlee014@gmail.com>
2023-11-05 23:28:16 -0800

    Started writing test cases

--- src/main/java/client/RecipeList/RecipeList.java
+++ src/main/java/client/RecipeList/RecipeList.java
@@ -44,6 +44,35 @@ public class RecipeList extends VBox {
         }
     }
 
+    public RecipeList(Recipe recipe) {
+        this.setSpacing(5);
+        this.setPrefSize(500, 560);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+
+        this.recipeList = new ArrayList<>();
+        /*
+         * dummy recipe objects
+         * {
+         * title: "Recipe 1"
+         * recipeDetail: "This is the first recipe"
+         * },
+         * {
+         * title: "Recipe 2"
+         * recipeDetail: "second recipe with a lot of lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus tellus vitae ex sollicitudin, quis eleifend nibh bibendum. Aliquam sit amet purus dui. Etiam tincidunt urna aliquam, imperdiet dolor id, blandit ante. Morbi porta elit sit amet fringilla egestas. Ut tincidunt lectus auctor ex sollicitudin feugiat. Integer congue tellus vitae nulla ornare, vitae gravida dolor pharetra. In eget dui orci. Nunc vestibulum nibh ac maximus ullamcorper. Integer dignissim accumsan neque vitae ultricies. Nunc tellus dolor, congue commodo orci tempus, lacinia congue leo. Donec fringilla, lectus vitae feugiat finibus, diam mauris egestas orci, in fringilla leo nulla sed velit. Praesent egestas euismod magna sed volutpat. Morbi consequat dignissim erat, sed mattis est suscipit vitae. Phasellus vitae sodales velit. \nPraesent blandit velit vel tristique eleifend. Phasellus ac est neque. Praesent lobortis gravida justo vel mollis. Vestibulum ut neque metus. Maecenas lacus mauris, pellentesque in urna rutrum, scelerisque porta libero. Praesent in rutrum sem. Quisque in ultrices ex. Cras mattis lacinia lacus, in sagittis lectus fermentum nec. Aenean commodo felis in quam semper sollicitudin. Sed ultricies faucibus velit, vitae tincidunt eros ullamcorper in. Pellentesque porta, purus at ultrices pharetra, odio mauris euismod neque, eu vehicula nisl odio id mi. Praesent maximus, erat sed porttitor varius, lorem turpis pellentesque sapien, vel scelerisque neque quam ut metus. Suspendisse tempus, nulla at mollis malesuada, metus est pellentesque ligula, vel elementum arcu odio id purus. Ut porta dictum turpis id commodo. Maecenas quis ex hendrerit, lacinia mi placerat, iaculis quam."
+         * }
+         */
+        String title1 = "Recipe 1";
+        String recipeDetail1 = "This is the first recipe";
+        String title2 = "Recipe 2";
+        String recipeDetail2 = "second recipe with a lot of lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus tellus vitae ex sollicitudin, quis eleifend nibh bibendum. Aliquam sit amet purus dui. Etiam tincidunt urna aliquam, imperdiet dolor id, blandit ante. Morbi porta elit sit amet fringilla egestas. Ut tincidunt lectus auctor ex sollicitudin feugiat. Integer congue tellus vitae nulla ornare, vitae gravida dolor pharetra. In eget dui orci. Nunc vestibulum nibh ac maximus ullamcorper. Integer dignissim accumsan neque vitae ultricies. Nunc tellus dolor, congue commodo orci tempus, lacinia congue leo. Donec fringilla, lectus vitae feugiat finibus, diam mauris egestas orci, in fringilla leo nulla sed velit. Praesent egestas euismod magna sed volutpat. Morbi consequat dignissim erat, sed mattis est suscipit vitae. Phasellus vitae sodales velit. \nPraesent blandit velit vel tristique eleifend. Phasellus ac est neque. Praesent lobortis gravida justo vel mollis. Vestibulum ut neque metus. Maecenas lacus mauris, pellentesque in urna rutrum, scelerisque porta libero. Praesent in rutrum sem. Quisque in ultrices ex. Cras mattis lacinia lacus, in sagittis lectus fermentum nec. Aenean commodo felis in quam semper sollicitudin. Sed ultricies faucibus velit, vitae tincidunt eros ullamcorper in. Pellentesque porta, purus at ultrices pharetra, odio mauris euismod neque, eu vehicula nisl odio id mi. Praesent maximus, erat sed porttitor varius, lorem turpis pellentesque sapien, vel scelerisque neque quam ut metus. Suspendisse tempus, nulla at mollis malesuada, metus est pellentesque ligula, vel elementum arcu odio id purus. Ut porta dictum turpis id commodo. Maecenas quis ex hendrerit, lacinia mi placerat, iaculis quam.";
+        this.recipeList.add(new RecipeListItem(new Recipe(title1, "Dinner", recipeDetail1)));
+        this.recipeList.add(new RecipeListItem(new Recipe(title2, "Lunch", recipeDetail2)));
+
+        for (RecipeListItem recipeListItem : this.recipeList) {
+            this.getChildren().add(recipeListItem);
+        }
+    }
+
     public List<RecipeListItem> getRecipeList() {
         return this.recipeList;
     }
@@ -53,7 +82,18 @@ public class RecipeList extends VBox {
         this.recipeList.add(0, recipeListItem);
         this.getChildren().add(0, recipeListItem);
     }
+
+    // public Recipe getRecipe(Recipe recipe){
+        // return this.getChildren().get(0);
+    // }
+
 }
+class RecipeListTest {
+    public RecipeListTest(){
+        
+    }
+}
+
 
 class RecipeListItem extends HBox {
     private Recipe recipe;
---
+++ src/test/java/client/AddRecipeTest.java
@@ -0,0 +1,26 @@
+package client;
+import client.RecipeList.RecipeList;
+import client.RecipeList.RecipeListView;
+import client.RecipeList.RecipeListHeader;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+public class AddRecipeTest {
+    @Test void addRecipeTitle(){
+        Recipe recipe = new Recipe("Chicken and Rice", "Dinner", "Its Chicken and Rice man...");
+        assertEquals(recipe.getTitle(), "Chicken and Rice");
+        // RecipeList lst = new RecipeList();
+        // lst.addRecipe(new Recipe("Chicken and Rice", "Dinner", "Its Chicken and Rice man..."));
+        // List<RecipeListItem> t = lst.getRecipeList();
+        // assertEquals("Chicken and Rice", ((Recipe) lst.getChildren()).getTitle());
+    }
+    @Test void recipeList(){
+        RecipeList lst = new RecipeList();
+        // RecipeList lst = new RecipeList();
+        // lst.addRecipe(new Recipe("Chicken and Rice", "Dinner", "Its Chicken and Rice man..."));
+        // List<RecipeListItem> t = lst.getRecipeList();
+        // assertEquals("Chicken and Rice", ((Recipe) lst.getChildren()).getTitle());
+    }
+}
