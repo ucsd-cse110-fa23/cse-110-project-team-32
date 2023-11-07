a99e6aa49b24298b10e1048f24ad9b2baabbb31e
Alexander Lee <alexanderlee014@gmail.com>
2023-11-06 16:23:18 -0800

    added another test case

--- src/main/java/client/AddRecipe/API.java
+++ src/main/java/client/AddRecipe/API.java
@@ -1,6 +1,8 @@
 package client.AddRecipe;
 
 public interface API {
+    //Maybe add a getInput method: That gets input
+    //Change the getInfo Method()
     public API initializeAPI(String API, String tokens, String model);
     public String getInfo();
 }
