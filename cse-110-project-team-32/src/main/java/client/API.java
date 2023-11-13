640c1dcb3d0c412a134cb4d4b72e94a3a781bbab
fli033 <fli033@ucr.edu>
2023-11-12 17:59:46 -0800

    remove useless files

--- cse-110-project-team-32/src/main/java/client/API.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-public interface API {
-    //Maybe add a getInput method: That gets input
-    //Change the getInfo Method()
-    public API initializeAPI(String API, String tokens, String model);
-    public String getInfo();
-}
