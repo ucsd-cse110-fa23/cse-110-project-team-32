dbf5864d02d9a95304f51ad1acf1eaa6de34214e
fli033 <fli033@ucr.edu>
2023-11-27 15:19:07 -0800

    fix user settings

--- src/main/java/client/UserSettings.java
+++ src/main/java/client/UserSettings.java
@@ -1,39 +1,37 @@
 package client;
 
 import java.io.*;
-import java.util.UUID;
-
-// First time users: creates a user id and store it in "user_uuid.txt" file
-// If not first time user: gets the uuid previously generated from "user_uuid.txt"
-// To retrieve data from sql table
 
 public class UserSettings {
 
   private static final String SETTINGS_FILE = "user_settings.txt";
   private String username;
-  private String password;
   private boolean autoLogin;
 
   public UserSettings() {
     // Try to read the UUID from the file
-    String settings = readUUIDFromFile();
+    String settings = readSettingsFromFile();
 
     // If no UUID is stored, generate a new one
     if (settings == null) {
-      settings = generateAndStoreUUID();
+      this.autoLogin = false;
+      this.username = null;
+    } else {
+      String[] settingsItems = settings.split("&");
+      this.autoLogin = settingsItems[0].split("=")[1].equals("true");
+      this.username = settingsItems[1].split("=")[1];
     }
-
-    String[] settingsItems = settings.split("&");
-    this.autoLogin = settingsItems[0].split("=")[1].equals("true");
-    this.username = settingsItems[1].split("=")[1];
-    this.password = settingsItems[2].split("=")[1];
   }
 
   public String getUsername() {
     return username;
   }
 
-  private String readUUIDFromFile() {
+  public boolean isAutoLoginOn() {
+    return this.autoLogin;
+  }
+
+  private String readSettingsFromFile() {
     try (
       BufferedReader reader = new BufferedReader(new FileReader(SETTINGS_FILE))
     ) {
@@ -42,17 +40,5 @@ public class UserSettings {
       e.printStackTrace();
       return null;
     }
-  }
-
-  private String generateAndStoreUUID() {
-    String newUUID = "user_" + UUID.randomUUID().toString();
-    try (
-      BufferedWriter writer = new BufferedWriter(new FileWriter(SETTINGS_FILE));
-    ) {
-      writer.write(newUUID);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return newUUID;
   }
 }
